11dbcae7920de85cc48745a40d45f202a6880d7b
tarakish <akr.3838soccer14@gmail.com>
2022-09-12 18:00:06 +0900

    Feat: メールのプレビューを実装

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def welcome
+		UserMailer.with(to: 'igarashi@examplle.com', name: 'igaiga').welcome
+  end
 end
