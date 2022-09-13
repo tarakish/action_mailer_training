51408b2eecc952b5ff4250e80aeec237b2f82975
tarakish <akr.3838soccer14@gmail.com>
2022-09-14 00:39:25 +0900

    Feat: usermailerの単体テストを追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
-
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'welcome' do
+    email = UserMailer.with(to: 'igarashi@example.com', name: 'igarashi').welcome
+    assert_emails(1){ email.deliver_now } # メールが送信キューに追加されるかどうか
+    assert_equal ['perfect_rails@example.com'], email.from
+    assert_equal ['igarashi@example.com'], mail.to
+    assert_equal '登録完了', email.subject
+    assetr_includes email.text_part.decoded, 'igarashi'
+    assetr_includes email.html_pard.decoded, 'igarashi'
+  end
 end
